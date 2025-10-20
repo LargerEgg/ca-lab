7fa52f9e9897acddb41caab06330effad84079cf
abdullah2056 <abdullah_2056@hotmail.com>
2025-10-20 13:57:44 -0400

    completed the LoggedInView file

--- src/main/java/view/LoggedInView.java
+++ src/main/java/view/LoggedInView.java
@@ -52,7 +52,13 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
         changePassword = new JButton("Change Password");
         buttons.add(changePassword);
 
-        logOut.addActionListener(this);
+        logOut.addActionListener(
+                evt ->{
+                    if (evt.getSource().equals(logOut)) {
+                        this.logoutController.execute();
+                    }
+                }
+        );
 
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
@@ -107,9 +113,7 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
      * React to a button click that results in evt.
      * @param evt the ActionEvent to react to
      */
-    public void actionPerformed(ActionEvent evt) {
-        System.out.println("Click " + evt.getActionCommand());
-    }
+    public void actionPerformed(ActionEvent evt) { System.out.println("Click " + evt.getActionCommand()); }
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
@@ -139,6 +143,6 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
     }
 
     public void setLogoutController(LogoutController logoutController) {
-        // TODO: save the logout controller in the instance variable.
+        this.logoutController = logoutController;
     }
 }
